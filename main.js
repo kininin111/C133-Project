cb76e592bd98676abcb440bf74ea5354dab5c8c9
kininin111 <87792936+kininin111@users.noreply.github.com>
2022-04-04 18:23:34 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+img = "";
+
+function preload()
+{
+
+}
+
+function setup()
+{
+    
+}
+
+function draw()
+{
+   
+}
+
+function addUser() 
+{
+    user_name = document.getElementById("page_1").value;
+    localStorage.setItem("page_1", page_1);
+    window.location = "page1.html"; 
+}
+function addUser() 
+{
+    user_name = document.getElementById("page_2").value;
+    localStorage.setItem("page_2", page_2);
+    window.location = "page2.html"; 
+}
+function addUser() 
+{
+    user_name = document.getElementById("page_3").value;
+    localStorage.setItem("page_3", page_3);
+    window.location = "page3.html"; 
+}
+function addUser() 
+{
+    user_name = document.getElementById("page_4").value;
+    localStorage.setItem("page_4", page_4);
+    window.location = "page4.html"; 
+}
